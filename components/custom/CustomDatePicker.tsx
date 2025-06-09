a25fcc336a6acb04daedbf7755b4eab8141203df
Masum Khan <masumkhan081@gmail.com>
2025-06-09 07:54:14 +0600

    commited

--- components/custom/CustomDatePicker.tsx
+++ components/custom/CustomDatePicker.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { Popover, PopoverContent } from '../ui/popover'
-import { PopoverTrigger } from '../ui/popover'
-import { Button } from '../ui/button'
-import { cn } from '@/0.lib/utils'
-import { CalendarIcon } from 'lucide-react'
+import React from "react";
+import { Popover, PopoverContent } from "../ui/popover";
+import { PopoverTrigger } from "../ui/popover";
+import { Button } from "../ui/button";
+import { cn } from "@/0.lib/utils";
+// import { Calendar } from "../ui/calendar";
+import { CalendarIcon } from "lucide-react";
 
 export default function CustomDatePicker({
   label,
   value,
-  onChange
+  onChange,
 }: {
-  label: string
-  value: Date
-  onChange: (date: Date | undefined) => void
+  label: string,
+  value: Date;
+  onChange: (date: Date | undefined) => void;
 }) {
   return (
     <div className="flex flex-col gap-2  ">
@@ -20,13 +21,13 @@ export default function CustomDatePicker({
       <Popover>
         <PopoverTrigger asChild>
           <Button
-            variant={'outline'}
+            variant={"outline"}
             className={cn(
-              'w-full bg-gray-200 !rounded-md pl-3 py-4 text-left font-normal',
-              'text-muted-foreground'
+              "w-full bg-gray-200 !rounded-md pl-3 py-4 text-left font-normal",
+              "text-muted-foreground"
             )}
           >
-            <span>{value?.toDateString() || 'Select Date'}</span>
+            <span>{value?.toDateString() || "Select Date"}</span>
             <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
           </Button>
         </PopoverTrigger>
@@ -35,11 +36,13 @@ export default function CustomDatePicker({
             mode="single"
             selected={value}
             onSelect={(date) => onChange(date)}
-            disabled={(date) => date > new Date() || date < new Date('1900-01-01')}
+            disabled={(date) =>
+              date > new Date() || date < new Date("1900-01-01")
+            }
             initialFocus
           /> */}
         </PopoverContent>
       </Popover>
     </div>
-  )
+  );
 }
