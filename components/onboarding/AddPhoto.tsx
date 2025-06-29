e38daf948ade6e8881892e91745299c802d1f143
Technext Limited <technext@Technexts-MacBook-Air.local>
2025-06-29 13:33:47 +0600

    commited

--- components/onboarding/AddPhoto.tsx
+++ components/onboarding/AddPhoto.tsx
@@ -5,8 +5,8 @@ type Props = {}
 
 export default function AddPhoto({}: Props) {
   return (
-    <div>
-      <EnhanceText txt="Add Photo" styleKey="appTitle" />
+    <div className="  w-48 h-48 rounded-full bg-gray-200 flex items-center justify-center cursor-pointer hover:bg-gray-300 transition-colors mx-auto">
+      <span className="text-gray-500">+ Add Photo</span>
     </div>
   )
 }
